80ba9bd4031fcf7435705e408fcd0e3a5b05e02f
Dan Thareja <danthareja@gmail.com>
2014-12-01 23:37:40 -0800

    adds new forEach

---
+++ newForEach.js
@@ -0,0 +1,29 @@
+/*
+
+JavaScript provides an Array.prototype.forEach method that allows you to iterate over array values. For this exercise you will create your own array method called 'each'. It will be similar to the forEach method, except for one difference. If the callback function returns true then the loop will stop and no additional values will be iterated.
+
+The following shows a contrived example of how this new method would be used:
+
+var letters = ['a', 'b', 'c', 'd', 'e']
+var allowedLetters = []
+letters.each(function(letter, index){
+  // break out of the loop if we reached a letter with the value 'd'
+  if(letter == 'd') {
+    return true;
+  }
+  allowedLetters.push(letter);   
+})
+
+// allowedLetters should equal ['a', 'b', 'c']
+
+*/
+
+Array.prototype.each = function(callback) {
+  for ( var i = 0; i < this.length; i++ ){
+    if (callback(this[i], i, this) !== undefined) return;
+  }
+};
+
+Array.prototype.each = function(callback) {
+  return this.some(callback);
+};
